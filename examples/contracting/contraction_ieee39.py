c8ba5d8cb1292c9e8b4c070ab6cb3e75026fd13d
Niccolò Andreetta <niccolo.andreetta@gmail.com>
2024-10-24 13:46:55 +0200

    Make freq calculation

--- examples/contracting/contraction_ieee39.py
+++ examples/contracting/contraction_ieee39.py
@@ -16,7 +16,8 @@ ps = dps.PowerSystemModel(model=model)
 ps.init_dyn_sim() # initialize power flow on the network
 print(max(abs(ps.state_derivatives(0, ps.x_0, ps.v_0))))
 
-t_end = 50 # simulation time
+t_end = 30 # simulation time
+t_event = 1 # time of the load step occurance
 t_0 = time.time()
 x_0 = ps.x_0.copy() # set the initial state as the one computes before
 
@@ -26,23 +27,21 @@ sol = dps_sol.ModifiedEulerDAE(ps.state_derivatives, ps.solve_algebraic, 0, x_0,
 t = 0
 res = defaultdict(list) # store the results
 
-# sc_bus_idx = ps.gen['GEN'].bus_idx_red['terminal'][0] # get the bus index of the generator where the short circuit occurs
-event_bus_idx = ps.loads['Load'].bus_idx_red['terminal'][1] # get the bus index of the generator where the short circuit occurs
-all_load_bus_idx = ps.loads['Load'].bus_idx_red['terminal'] # index of all the buses
+event_load_bus_idx = ps.loads['Load'].bus_idx_red['terminal'][1] # bus index of the generator where the P step
+all_load_bus_idx = ps.loads['Load'].bus_idx_red['terminal'] # index of all the loads
 
-# ps.gen['GEN'] -> access all the generators in the network
-# ps.gen['GEN'].bus_idx_red['terminal'][0] -> get the bus index of the generator where the short circuit occurs
-s_const = ps.loads['Load'].par['P'] + 1j * ps.loads['Load'].par['Q'] # "old" apparent power
+s_const_old = (ps.loads['Load'].par['P'] + 1j * ps.loads['Load'].par['Q'])/ps.s_n # "old" apparent power
 v_old = ps.v_0[all_load_bus_idx]
-y_old = abs(v_old)**2*np.conj(s_const) # admittance of the load
+y_old = np.conj(s_const_old)/abs(v_old)**2 # admittance of the load
+v = ps.v_0
         
 while t < t_end:
   sys.stdout.write("\r%d%%" % (t/(t_end)*100)) # print the percentage of the simulation completed
 
   # Implement the short circuit in the bus where the generator is connected
-  #if event:
-  #  s_const[idx_event] += 1e2
-
+  if t > t_event and t < t_event +5e-3:
+   s_const_old[event_load_bus_idx] += 1e2/ps.s_n 
+ 
   # Simulate next step
   result = sol.step() # integrate the system one step
   # Extract the information from the solution
@@ -51,8 +50,13 @@ while t < t_end:
   t = sol.t
 
   # Constant power loads: update the modified admittance matrix
+  # tmp = ps.y_bus_red_full[3,2]
+  # Igen_43 = ps.y_bus_red_full[3,2]*(v[3] - v[2])
+  # Igen_45 = ps.y_bus_red_full[3,4]*(v[3] - v[4])
+  # Igen_4_14 = ps.y_bus_red_full[3,13]*(v[3] - v[13])
+  # s_43 = v[3]*np.conj(Igen_43 + Igen_45)
   v_load = v[all_load_bus_idx]
-  y_new = abs(v_load)**2*np.conj(s_const) # new admittance of the load
+  y_new = np.conj(s_const_old)/abs(v_load)**2 # new admittance of the load
   ps.y_bus_red_mod[(all_load_bus_idx,) * 2] = y_new - y_old
 
   dx = ps.ode_fun(0, ps.x_0) # compute the derivative of the state variables (in case they are needed)
