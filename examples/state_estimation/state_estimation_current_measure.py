353f13d69b98307066a7360a52439880466b3e53
Niccolò Andreetta <niccolo.andreetta@gmail.com>
2025-02-05 12:16:10 +0100

    Estimation based on the current for all the bus

--- examples/state_estimation/state_estimation_current_measure.py
+++ examples/state_estimation/state_estimation_current_measure.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
   # Power system model
   ps = dps.PowerSystemModel(model=model)
   ps.init_dyn_sim()
-  # print(max(abs(ps.state_derivatives(0, ps.x_0, ps.v_0))))
 
   t_end = 10
   iter = 1
@@ -30,85 +29,83 @@ if __name__ == '__main__':
   t = 0
   res = defaultdict(list)
   t_0 = time.time()
-  # Model of the sensor
-  H_mag   = np.eye(ps.n_bus) # Identity matrix
-  H_angle = np.eye(ps.n_bus) # Identity matrix
-  H = np.block([[H_angle, np.zeros_like(H_angle)], [np.zeros_like(H_angle), H_mag]])
+
+  # Simulate next step
+  sol = dps_sol.ModifiedEulerDAE(ps.state_derivatives, ps.solve_algebraic, 0, x_0, t_end, max_step=5e-3)
+  result = sol.step()
+
+  # Real values of bus voltage and line currents
+  x = sol.v
+  i_bus = ps.y_bus_lf @ x  
+  x_Rv = np.real(x)     # real part of the voltage
+  x_Iv = np.imag(x)     # imaginary part of the voltage
+  x_Ri = np.real(i_bus) # real part of the current
+  x_Ii = np.imag(i_bus) # imaginary part of the current
+  X    = np.hstack([x_Rv, x_Iv]) # vector of all the measures
+
   # Noise
-  R_angle     = 1e3*np.eye(ps.n_bus)  # Identity matrix
-  R_mag       = 1e3*np.eye(ps.n_bus)  # Identity matrix
-  R_prev      = np.block([[R_angle, np.zeros_like(R_angle)], [np.zeros_like(R_angle), R_mag]])
-  # Initial state
-  x_v_angles  = 0*np.ones(ps.n_bus)
-  x_v_mag     = 0*np.ones(ps.n_bus)
-  X_prev      = np.concatenate((x_v_angles, x_v_mag), axis=0).reshape(-1, 1)
-  P_prev      = np.linalg.inv(H.T @ np.linalg.inv(R_prev) @ H)
+  mu_v  = 0*np.ones(ps.n_bus)
+  sigma_v  = 0.01
+  mu_i  = 0*np.ones(ps.n_bus)
+  sigma_i  = 1e-2*0.01
+  R_Rv  = sigma_v*np.eye(ps.n_bus)  # Covariance on the measure on the real part of v
+  R_Iv  = sigma_v*np.eye(ps.n_bus)  # Covariance on the measure on the imaginary part of v
+  R_v   = np.block([[R_Rv, np.zeros_like(R_Rv)], [np.zeros_like(R_Iv), R_Iv]])
+  R_Ri  = sigma_i*np.eye(ps.n_bus)  # Covariance on the measure on the real part of i
+  R_Ii  = sigma_i*np.eye(ps.n_bus)  # Covariance on the measure on the imaginary part of i
+  R_i   = np.block([[R_Ri, np.zeros_like(R_Ri)], [np.zeros_like(R_Ii), R_Ii]])
+  R     = np.block([[R_v, np.zeros_like(R_v)], [np.zeros_like(R_i), R_i]]) # Covariance matrix of all the measures
 
-  for j in range(iter): 
-    # Simulate next step
-    sol = dps_sol.ModifiedEulerDAE(ps.state_derivatives, ps.solve_algebraic, 0, x_0, t_end, max_step=5e-3)
-    result = sol.step()
-    x = sol.y
-    v = sol.v
-    t = sol.t
+  epsilon_Rv  = np.random.multivariate_normal(mean=mu_v, cov=R_Rv, size=1)
+  epsilon_Iv  = np.random.multivariate_normal(mean=mu_v, cov=R_Iv, size=1)
+  epsilon_Ri  = np.random.multivariate_normal(mean=mu_i, cov=R_Ri, size=1)
+  epsilon_Ii  = np.random.multivariate_normal(mean=mu_i, cov=R_Ii, size=1)
+  epsilon     = np.hstack([epsilon_Rv, epsilon_Iv, epsilon_Ri, epsilon_Ii])
+  
+  # Model of the sensor
+  H_Rv    = np.eye(ps.n_bus) # Identity matrix
+  H_Iv    = np.eye(ps.n_bus) # Identity matrix
+  H_v     = np.block([[H_Rv, np.zeros([ps.n_bus,ps.n_bus])], [np.zeros([ps.n_bus,ps.n_bus]), H_Iv]])
+  H_RiRv  = np.real(ps.y_bus_lf)
+  H_RiIv  = -np.imag(ps.y_bus_lf)
+  H_IiRv  = np.imag(ps.y_bus_lf)
+  H_IiIv  = np.real(ps.y_bus_lf)
+  H_i     = np.block([[H_RiRv, H_RiIv], [H_IiRv, H_IiIv]])
+  H       = np.vstack([H_v, H_i]) # Measures angles and magnitudes
 
-    i_bus = ps.y_bus_lf @ v # Line Currents
+  # Measures
+  z = H @ X + epsilon
+  z_v = z[0][0:ps.n_bus] + 1j*z[0][ps.n_bus:2*ps.n_bus]
+  z_i = z[0][2*ps.n_bus:3*ps.n_bus] + 1j*z[0][3*ps.n_bus:4*ps.n_bus]
 
-    # Noise
-    epsilon_angle_mean = 0*np.ones(ps.n_bus)
-    epsilon_angle_std = 1e-6*0.01
-    epsilon_mag_mean = 0*np.ones(ps.n_bus)
-    epsilon_mag_std = 1e-6*0.01
-    R_angle = epsilon_angle_std*np.eye(ps.n_bus)  # Identity matrix
-    R_mag = epsilon_mag_std*np.eye(ps.n_bus)      # Identity matrix
-    R = np.block([[R_angle, np.zeros_like(R_angle)], [np.zeros_like(R_angle), R_mag]])
+  # Estimate the state
+  G = H.T @ np.linalg.inv(R) @ H
+  x_hat = np.linalg.inv(G) @ H.T @ np.linalg.inv(R) @ z.T
+  x_hat_comb = x_hat[0:ps.n_bus] + 1j*x_hat[ps.n_bus:2*ps.n_bus]
 
-    # True angles and magnitudes
-    x_v_angles = np.arctan2(np.imag(v), np.real(v)) # Bus angles
-    x_v_mag = np.abs(v) # Bus voltage magnitudes
-    X = np.concatenate((x_v_angles, x_v_mag), axis=0).reshape(-1, 1)
+  G_vmi = H_i.T @ np.linalg.inv(R_i) @ H_i # estimate v measuring i
+  x_hat_vmi = np.linalg.inv(G_vmi) @ H_i.T @ np.linalg.inv(R_i) @ z[0][2*ps.n_bus:4*ps.n_bus].T
+  x_hat_comb_vmi = x_hat_vmi[0:ps.n_bus] + 1j*x_hat_vmi[ps.n_bus:2*ps.n_bus]
 
-    # Measures angles and magnitudes
-    epsilon_angle = np.random.multivariate_normal(mean=epsilon_angle_mean, cov=R_angle, size=1)
-    epsilon_mag = np.random.multivariate_normal(mean=epsilon_mag_mean, cov=R_mag, size=1)
-    epsilon = np.concatenate((epsilon_angle.T, epsilon_mag.T), axis=0).reshape(-1, 1)
-    z = H@X + epsilon
-
-    i_noise = i_bus + 10*epsilon_mag
-    v_noise = np.linalg.inv(ps.y_bus_lf)@i_noise.T # voltage measure with noise
-
-    x_hat = np.linalg.inv(H.T @ np.linalg.inv(R) @ H) @ H.T @ np.linalg.inv(R) @ z
-
-    S = H@P_prev@H.T + R_prev
-    W = P_prev@H.T*np.linalg.inv(S)
-    X_hat_tmp = X_prev + W@(z - H@X_prev) # new estimation
-    P_hat_tmp = P_prev - W@H@P_prev           # new covariance
-
-    res['X_hat'].append(X_hat_tmp)
-    res['P_hat'].append(P_hat_tmp)
-    res['v'].append(v)
-
-    X_prev = X_hat_tmp
-    P_prev = P_hat_tmp
-
-  X_angle_tmp = np.concatenate([X_hat[0] for X_hat in res['X_hat']])
-  X_angle_tmp = X_angle_tmp*180/np.pi
-  X_mag_tmp = np.concatenate([X_hat[39] for X_hat in res['X_hat']])
-  v_tmp = [v[0] for v in res['v']]
-  v_tmp_mag = np.abs(v_tmp)
-  v_tmp_angle = np.arctan2(np.imag(v_tmp), np.real(v_tmp))*180/np.pi
-
-  names = list(range(1, iter+1))
+  # Plot
+  names = list(range(1, ps.n_bus+1))
   plt.figure()
-  plt.plot(names, v_tmp_angle, 'o')
-  plt.plot(names, X_angle_tmp, 'x')
-  plt.xlabel('Generator')
-  plt.ylabel('Angle [deg]')
-  plt.show()
-
+  plt.plot(names, np.abs(x), 'o', label='Real values') 
+  plt.plot(names, np.abs(x_hat_comb), 'x', label='Estimated values')
+  plt.plot(names, np.abs(z_v), 'v', label='v measure from v')     # v measuring v
+  plt.plot(names, np.abs(x_hat_comb_vmi), '1',  label='v measure from i') # v measuring i
+  plt.xlabel('Bus number')  
+  plt.ylabel('Mag [-]')
+  plt.legend()
+  
   plt.figure()
-  plt.plot(names, v_tmp_mag, 'o')
-  plt.plot(names, X_mag_tmp, 'x')
-  plt.xlabel('Generator')
-  plt.ylabel('Voltage [pu]')
+  plt.plot(names, np.arctan2(np.imag(x),np.real(x)), 'o', label='Real values') 
+  plt.plot(names, np.arctan2(np.imag(x_hat_comb),np.real(x_hat_comb)), 'x', label='Estimated values')
+  plt.plot(names, np.arctan2(np.imag(z_v),np.real(z_v)), 'v', label='v measure from v')     # v measuring v
+  plt.plot(names, np.arctan2(np.imag(x_hat_comb_vmi),np.real(x_hat_comb_vmi)), '1',  label='v measure from i') # v measuring i
+  plt.xlabel('Bus number')  
+  plt.ylabel('Angle [-]')
+  plt.legend()
+
+
   plt.show()
